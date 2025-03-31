ec2a7b83aca292eca493c88f79aab486d148767f
matiasfelau <matiasfelau@gmail.com>
2025-03-31 19:03:01 -0300

    agregado error local y global a lagrange

--- procedimientos/lagrange.py
+++ procedimientos/lagrange.py
@@ -1,17 +1,26 @@
 import sympy as sp
 
-from tools.analisis_matematico import x
+from procedimientos.biseccion import calcular_punto_medio
+from procedimientos.newton_raphson import calcular
+from tools.analisis_matematico import x, evaluar_funcion, derivar_funcion, calcular_punto_maximo, \
+    calcular_raices_intervalo
 from tools.logger import console_log
 from utilities.enumerations import LogTypes
 
-c = [[1, 1], [2, 4], [3, 9]]  # conjunto de puntos
+c = [[0, 0], [sp.pi/2, sp.sin(sp.pi/2)], [sp.pi, sp.sin(sp.pi)]]  # conjunto de puntos
+fx = sp.sympify('sin(x)') #funcion
+p = sp.pi/3
 
-def ejecutar(conjunto_puntos):
+def ejecutar(conjunto_puntos, funcion, punto_evaluado_error):
     try:
         resultado_crudo = iterar(conjunto_puntos)
         console_log(LogTypes.VAR, f'P(x) = {resultado_crudo}')
         resultado_final = sp.expand(resultado_crudo)
         print(f'EL POLINOMIO ES: {resultado_final}')
+        error_local = calcular_error_local(funcion, resultado_final, punto_evaluado_error)
+        print(f'EL ERROR LOCAL ES: {error_local}')
+        error_global = calcular_error_global(funcion, conjunto_puntos)
+        print(f'EL ERROR GLOBAL ES: {error_global}')
     except Exception as e:
         console_log(LogTypes.ERROR, str(e))
 
@@ -32,5 +41,37 @@ def iterar(conjunto_puntos):
     except Exception as e:
         console_log(LogTypes.ERROR, str(e))
 
+def calcular_error_local(funcion, polinomio, punto):
+    try:
+        return abs(evaluar_funcion(funcion, punto) - evaluar_funcion(polinomio, punto))
+    except Exception as e:
+        console_log(LogTypes.ERROR, str(e))
+
+def calcular_error_global(funcion, conjunto_puntos):
+    try:
+        cantidad_nodos = len(conjunto_puntos)
+        derivada_funcion = funcion
+        for i in range(cantidad_nodos):
+            derivada_funcion = derivar_funcion(derivada_funcion)
+        termino_error = funcion / sp.factorial(cantidad_nodos)
+        punto_inicial = conjunto_puntos[0]
+        coordenada_x_punto_inicial = punto_inicial[0]
+        punto_final = conjunto_puntos[-1]
+        coordenada_x_punto_final = punto_final[0]
+        intervalo = [coordenada_x_punto_inicial, coordenada_x_punto_final]
+        punto_maximo = calcular_punto_maximo(funcion, intervalo)
+        termino_error = abs(evaluar_funcion(termino_error, punto_maximo))
+        termino_productivo = 1
+        for punto in conjunto_puntos:
+            coordenada_x_punto = punto[0]
+            termino_productivo *= (x - coordenada_x_punto)
+        derivada_termino_productivo = derivar_funcion(termino_productivo)
+        raices = calcular_raices_intervalo(derivada_termino_productivo, intervalo)
+        ultima_raiz = raices.args[-1]
+        termino_productivo = abs(evaluar_funcion(termino_productivo, ultima_raiz))
+        return termino_error * termino_productivo
+    except Exception as e:
+        console_log(LogTypes.ERROR, str(e))
+
 if __name__ == '__main__':
-    ejecutar(c)
+    ejecutar(c, fx, p)
--- tools/analisis_matematico.py
+++ tools/analisis_matematico.py
@@ -22,3 +22,28 @@ def calcular_error(ultimo_resultado, resultado_anterior):
         return round(abs(ultimo_resultado - resultado_anterior), 9)
     except Exception as e:
         console_log(LogTypes.ERROR, str(e))
+
+def calcular_punto_maximo(funcion, intervalo):
+    try:
+        primer_derivada = derivar_funcion(funcion)
+        puntos_criticos = calcular_raices_intervalo(primer_derivada, intervalo)
+        segunda_derivada = derivar_funcion(primer_derivada)
+        fue_encontrado = False
+        for punto in puntos_criticos:
+            if evaluar_funcion(segunda_derivada, punto) < 0:
+                fue_encontrado = True
+                break
+        if fue_encontrado:
+            return punto
+        else:
+            print('No se encontro')
+    except Exception as e:
+        console_log(LogTypes.ERROR, str(e))
+
+def calcular_raices_intervalo(funcion, intervalo):
+    try:
+        inicio_intervalo = intervalo[0]
+        final_intervalo = intervalo[1]
+        return sp.solveset(funcion, x, domain=sp.Interval(inicio_intervalo, final_intervalo))
+    except Exception as e:
+        console_log(LogTypes.ERROR, str(e))
